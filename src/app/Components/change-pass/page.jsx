6fd80e2d14e142672c6fb49b9252f52c806cfb1f
email <sahushankar8202@gmail.com>
2023-12-02 22:15:59 +0530

    change pass

---
+++ src/app/Components/change-pass/page.jsx
@@ -0,0 +1,33 @@
+
+import React from "react";
+
+const ChangePass = ({ isOpen, onClose, onHandleChange }) => {
+  return (
+    <div
+      className={`fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 ${
+        isOpen ? "block" : "hidden"
+      }`}
+    >
+      <div className="absolute top-0 left-0 w-full h-full bg-gray-800 opacity-50"></div>
+      <div className="bg-white p-4 rounded-md z-10">
+        <p className="text-xl font-semibold mb-4">Are you sure you want to change the password ?</p>
+        <div className="flex justify-center">
+          <button
+            onClick={onClose}
+            className="bg-gray-500 text-white px-4 py-2 font-semibold rounded-md mr-2"
+          >
+            No
+          </button>
+          <button
+            onClick={onHandleChange}
+            className="bg-[#FFBF00] text-white font-semibold px-4 py-2 rounded-md"
+          >
+            Yes
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChangePass;
--- src/app/Components/pop-up-allusers/page.jsx
+++ src/app/Components/pop-up-allusers/page.jsx
@@ -4,10 +4,15 @@ import Image from "next/image";
 import axios from "axios";
 import Link from "next/link";
 import styles from "../pop-up.module.css";
+import ChangePass from "../change-pass/page";
 
 function UserProfileModal({ user, onClose }) {
   const [users, setUsers] = useState([]);
   const [isLinkCopied, setIsLinkCopied] = useState(false);
+  const [openInput, setOpeninput] = useState(false);
+  const [openConfirmationPass, setOpenConfirmationPass] = useState(false);
+  const [newPassword, setNewPassword] = useState("");
+  const [confirm,setConfirm]=useState()
   // console.log(user,"id")
   // -----------------------------------------
   useEffect(() => {
@@ -42,7 +47,7 @@ function UserProfileModal({ user, onClose }) {
       await navigator.clipboard.writeText(users?.wishlist[index]?.link);
       setIsLinkCopied(true);
     } catch (error) {
-      console.error('Error copying link to clipboard:', error);
+      console.error("Error copying link to clipboard:", error);
     }
   };
   const truncateLink = (link) => {
@@ -52,7 +57,49 @@ function UserProfileModal({ user, onClose }) {
     );
     return truncatedWords.join(" ");
   };
+  function HandleOpenInput() {
+    setOpeninput(!openInput);
+  }
+  function handleOpenModal() {
+    setOpenConfirmationPass(true);
+  }
+  const handleCancelChange = () => {
+    setOpenConfirmationPass(false);
+  };
+console.log(newPassword,"newpass")
+console.log(user,"user")
+  const handleChangePassword = async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+  
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/admin_change_user_password`,
+        {
+          id: user, // Assuming user is the variable holding the user ID
+          pw: newPassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
+      if (response.data.status_code === 200) {
+        setConfirm(true)
+        alert("Password Change successfully")
+      }
+  else{
+    setConfirm(false)
+  }
+      setUsers(response.data.data);
+    } catch (error) {
+      console.error("Error updating password:", error);
+    }
+    setOpenConfirmationPass(false);
+  };
+  
+  
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center mx-auto max-w-screen-3xl ">
       <div
@@ -74,7 +121,7 @@ function UserProfileModal({ user, onClose }) {
           <div className="flex rounded-full justify-center pt-[50px]">
             <Image
               src={users?.profile_pic || "/na.png"}
-              alt={`${users?.name}`}  
+              alt={`${users?.name}`}
               width={150}
               height={150}
               className="rounded-full"
@@ -243,26 +290,30 @@ function UserProfileModal({ user, onClose }) {
                     <div className=""> {data?.name}</div>
                   </div>
                   <div className="flex md:justify-between justify-center p-2">
-      <div className="p-[5px] mr-1" onClick={() => handleCopyLink(index)} style={{ cursor: 'pointer' }}>
-        <Image
-          src="/link.png"
-          width={15}
-          height={15}
-          alt="image-link"
-        />
-      </div>
-      <div className="overflow-hidden w-[200px] h-[50px]">
-        {data?.link ? (
-          <Link href={data?.link} className="text-[#767676]">
-                   <span className="overflow-hidden">{truncateLink(data?.link)}</span>
-
-          </Link>
-        ) : (
-          "N/A"
-        )}
-      </div>
-     
-    </div>
+                    <div
+                      className="p-[5px] mr-1"
+                      onClick={() => handleCopyLink(index)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      <Image
+                        src="/link.png"
+                        width={15}
+                        height={15}
+                        alt="image-link"
+                      />
+                    </div>
+                    <div className="overflow-hidden w-[200px] h-[50px]">
+                      {data?.link ? (
+                        <Link href={data?.link} className="text-[#767676]">
+                          <span className="overflow-hidden">
+                            {truncateLink(data?.link)}
+                          </span>
+                        </Link>
+                      ) : (
+                        "N/A"
+                      )}
+                    </div>
+                  </div>
                   <div className="flex md:justify-between justify-center    p-2">
                     <div className=" p-[2px]  mr-1">
                       <Image
@@ -301,7 +352,67 @@ function UserProfileModal({ user, onClose }) {
               </div>
             </div>
           </div>
+          {/* -------------change password------------------------ */}
+
+          <div className="flex my-5 gap-1">
+            <button
+              onClick={HandleOpenInput}
+              className="relative inline-flex items-center justify-center px-3 py-2 overflow-hidden  font-medium tracking-tighter hover:font-semibold  text-black bg-[#F6F6F6] rounded-lg group "
+            >
+              <span className="absolute w-0 h-0 transition-all duration-500  ease-out bg-[#FFBF001F]    rounded-full group-hover:w-56 group-hover:h-56"></span>
+              <span className="absolute inset-0 w-full h-full -mt-1  rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent "></span>
+              <span className="relative">Change Password</span>
+            </button>
+
+            {openInput && (
+              <>
+                <input
+                  type="text"
+                  id="text"
+                  name="text"
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  className=" border border-gray-300 px-1 py-1 rounded-lg focus:outline-none focus:border-[#FFBF00]"
+                  placeholder="Enter new password"
+                />
+                <button
+                  onClick={handleOpenModal}
+                  className="relative inline-flex items-center justify-center px-2 py-2 overflow-hidden  font-medium tracking-tighter hover:font-semibold  text-black bg-[#F6F6F6] rounded-lg group "
+                >
+                  <span className="absolute w-0 h-0 transition-all duration-500  ease-out bg-[#FFBF001F]    rounded-full group-hover:w-56 group-hover:h-56"></span>
+                  <span className="absolute inset-0 w-full h-full -mt-1  rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent "></span>
+                  <span className="relative">Update Password</span>
+                  <svg
+                    class="w-4 h-4 ml-2"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M14 5l7 7m0 0l-7 7m7-7H3"
+                    ></path>
+                  </svg>
+                </button>{" "}
+              </>
+            )}
+          
+
+          </div>
+          {/* <div>
+           {confirm && (<div className="text-green-500 font-semibold">
+            Password Change Successfully
+           </div>)  }
+           
+           </div> */}
         </div>
+        <ChangePass
+          isOpen={openConfirmationPass}
+          onClose={handleCancelChange}
+          onHandleChange={handleChangePassword}
+        />
       </div>
     </div>
   );
